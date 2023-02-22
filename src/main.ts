e164014196cd4d32c1805fef7baa24933b5765f2
Duong Le <navi.ocean@outlook.com>
2023-02-22 14:45:24 +0700

    chore: update process

--- src/main.ts
+++ src/main.ts
@@ -2,32 +2,32 @@ import Node from './node';
 
 const node = new Node();
 
-// const gracefulShutdown = (): void => {
-//   console.log('');
-//   console.error(
-//     'xxx',
-//     'sys',
-//     'Received kill signal, shutting down gracefully.',
-//   );
+const gracefulShutdown = (): void => {
+  console.log('');
+  console.error(
+    'xxx',
+    'sys',
+    'Received kill signal, shutting down gracefully.',
+  );
 
-//   node.stop();
-//   console.info('xxx', 'sys', 'Closed node watcher');
+  node.stop();
+  console.info('xxx', 'sys', 'Closed node watcher');
 
-//   setTimeout(function () {
-//     console.info('xxx', 'sys', 'Closed out remaining connections.');
-//     process.exit(0);
-//   }, 1000);
-// };
+  setTimeout(function () {
+    console.info('xxx', 'sys', 'Closed out remaining connections.');
+    process.exit(0);
+  }, 1000);
+};
 
-// // listen for TERM signal .e.g. kill
-// process.on('SIGTERM', gracefulShutdown);
+// listen for TERM signal .e.g. kill
+process.on('SIGTERM', gracefulShutdown);
 
-// // listen for INT signal e.g. Ctrl-C
-// process.on('SIGINT', gracefulShutdown);
+// listen for INT signal e.g. Ctrl-C
+process.on('SIGINT', gracefulShutdown);
 
-// // listen for shutdown signal from pm2
-// process.on('message', function (msg) {
-//   if (msg == 'shutdown') gracefulShutdown();
-// });
+// listen for shutdown signal from pm2
+process.on('message', function (msg) {
+  if (msg == 'shutdown') gracefulShutdown();
+});
 
 export default node;
