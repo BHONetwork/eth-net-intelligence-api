22e323c70441197ac668ce1c4a32c9df01f465b1
Duong Le <navi.ocean@outlook.com>
2023-02-22 18:18:19 +0700

    chore: clean

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
 // import * as os from 'os';
 import * as dotenv from 'dotenv';
-import process from 'process';
 dotenv.config();
 export const WS_SERVER = process.env.WS_SERVER || 'ws://localhost:3000';
 export const RPC_HOST = process.env.RPC_HOST || 'http://localhost:8545';
@@ -13,16 +12,12 @@ export const MAX_BLOCKS_HISTORY = 40;
 export const UPDATE_INTERVAL = 30000;
 export const PING_INTERVAL = 3000;
 export const SYNC_INTERVAL = 5000;
-
 export const MAX_HISTORY_UPDATE = 50;
 export const MAX_CONNECTION_ATTEMPTS = 50;
 export const CONNECTION_ATTEMPTS_TIMEOUT = 1000;
 
 export const ENABLE_HISTORY = process.env.ENABLE_HISTORY || true;
-
 export const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL || '';
-
 export const DEFAULT_RPC_HOST =
   process.env.DEFAULT_RPC_HOST || 'https://bsc-dataseed1.defibit.io';
-
 export const DIFF_BLOCK = process.env.DIFF_BLOCK || 30;
